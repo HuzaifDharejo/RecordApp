68b0f20be7e73feea8719da8e1a1135ca749372f
HuzaifDharejo <huzaifdharejo@gmail.com>
2020-08-05 10:41:23 +0500

    worked on form in home view

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,7 @@
 exports.get_home = function(req, res, next) {
     res.render('home', { title: 'Home' });
 }
+exports.submit_lead = function(req, res, next) {
+    console.log("Lead Email :", req.body.lead_email);
+    res.redirect('/');
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let home = require('../controllers/home');
 /* GET home page. */
 router.get('/', home.get_home);
+router.post('/', home.submit_lead);
 
 module.exports = router;
